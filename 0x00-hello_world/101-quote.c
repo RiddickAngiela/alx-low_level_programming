b7af22e72f9cc9b4824199b4e47764c4c313c819
RiddickAngiela <angielariddick@gmail.com>
2023-02-16 14:28:31 +0300

    Task Completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,11 +13,11 @@ long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
- __asm__("syscall"
-        : "=a"(ret)
-            : "a"(syscall),
-              "D"(fd),
-              "S"(s),
-              "d"(l));
+__asm__("syscall"
+: "=a"(ret)
+: "a"(syscall),
+"D"(fd),
+"S"(s),
+"d"(l));
 return (1);
 }
